da9140bc093f9a22a2537722173da925b1947550
Prashant Gotra <prashantgotra71@gmail.com>
2022-01-13 12:07:42 +0530

    Add files via upload

---
+++ src/MyComponents/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            This is an about component
+            <p>This a todos app, user can create any number of cards,it help you stay organized and manage your day-to-day
+                and therefore increases your productivity and focus on what matters to you.
+            </p>
+        </div>
+    )
+}
